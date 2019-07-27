2d8b748722a2120b8e2a4e7425debc70519b7c41
EquipoP <44071007+EquipoP@users.noreply.github.com>
2019-07-26 22:50:28 -0300

    prefinal

--- lamaquina.js
+++ lamaquina.js
@@ -5,12 +5,31 @@ function verifyAns() {
     var inputNum = document.getElementById("input1").value;
     inputNum = inputNum.concat(document.getElementById("input2").value,document.getElementById("input3").value,document.getElementById("input4").value)
     console.log(inputNum);
-    if (inputNum == '1998'){
+    if (inputNum == '4338'){
         failMSG1.style.display = "";
         failMSG2.style.display = "";
         document.getElementById("success").style.display= "inline-block";
-        document.getElementById("success").innerHTML = "RESPUESTA CORRECTA, LA LLAVE ESTÁ ABAJO DE LA MESA";
-    }else{
+        document.getElementById("success").innerHTML = success1;
+    }
+    else if (inputNum == '4358'){
+        failMSG1.style.display = "";
+        failMSG2.style.display = "";
+        document.getElementById("success").style.display= "inline-block";
+        document.getElementById("success").innerHTML = success2;
+    }
+    else if (inputNum == '4368'){
+        failMSG1.style.display = "";
+        failMSG2.style.display = "";
+        document.getElementById("success").style.display= "inline-block";
+        document.getElementById("success").innerHTML = success3;
+    }
+    else if (inputNum == '4398'){
+        failMSG1.style.display = "";
+        failMSG2.style.display = "";
+        document.getElementById("success").style.display= "inline-block";
+        document.getElementById("success").innerHTML = success4;
+    }
+    else{
         document.getElementById("success").style.display= "";
         if (failMSG1.style.display == ""){
             failMSG1.style.display = "inline-block";
@@ -30,3 +49,8 @@ function sleep(milliseconds) {
     }
   }
 }
+
+var success1 = "CORRECTO, SI USTED ESTÁ EN LA EX-SECRETARÍA LA LLAVE ESTÁ EN 1";
+var success2 = "CORRECTO, SI USTED ESTÁ EN EL VESTUARIO FEMENINO LA LLAVE ESTÁ EN 2"
+var success3 = "CORRECTO, SI USTED ESTÁ EN LA EX-DIRECCIÓN LA LLAVE ESTÁ EN 3"
+var success4 = "CORRECTO, SI USTED ESTÁ EN LA SALA DEL FONDO LA LLAVE ESTÁ EN 4"
